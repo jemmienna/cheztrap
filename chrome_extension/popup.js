c5bee01344fae3d0d484e2344c0a7bd214ae48a7
Jiena Lee <december0two@gmail.com>
2024-07-10 22:27:31 -0700

    Got the chrome extension to work to a certain extent.

---
+++ chrome_extension/popup.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      const currentUrl = currentTab.url;
+  
+      const iframe = document.getElementById('iframe');
+      if (iframe) {
+        iframe.src = "http://127.0.0.1:5000/check/result/chrome?url="+currentUrl;
+      } else {
+        console.error('Element with ID "url" is not an iframe.');
+      }
+    });
+  });
+  
--- home.py
+++ home.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 #shortens input url to just its domain and path
-#www.youtube.com --> youtube.com
+#ex: www.youtube.com --> youtube.com
 def strip_scheme(url):
     parsed_url = urlparse(url)
     netloc = parsed_url.netloc
@@ -71,6 +71,40 @@ def result():
 @app.route('/check?error=invalid_url')
 def go_back():
     return redirect('/check')
+
+@app.route('/check/result/chrome')
+def chrome_result():
+    #sets the url query param
+    #/check/result?url=
+    url = request.args.get('url')
+    stripped_url = strip_scheme(url)
+
+    #sends the form input to urlvoid
+    data = {
+        'site': stripped_url
+    }
+
+    page_to_scrape = requests.post('https://www.urlvoid.com', data=data)
+
+    soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+
+    detections = 0
+    detections = soup.findAll("span", attrs={"class":"label"})
+    #if length of detections is equal to 0, an error will pop up because the list is empty
+    if len(detections) == 0:
+        return redirect('/check?error=invalid_url')
+    detection_count = detections[0].text
+
+    #if detection count from urlvoid is 0, then site is safe
+    #otherwise, site might be dangerous
+    if detection_count == "0/40":
+        print("Safe")
+    else:
+        print("Might be dangerous")
+    print(detection_count)
+
+    return render_template('check_result_chrome.html', detection_count=detection_count, url=stripped_url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
