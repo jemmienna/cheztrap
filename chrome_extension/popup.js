7d1e81b04b481f2d17d2517baf355a58f92493b6
Jiena Lee <december0two@gmail.com>
2024-07-27 21:05:32 -0700

    updated and fixed chrome extension to work

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,13 +1,28 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const currentTab = tabs[0];
-      const currentUrl = currentTab.url;
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    const currentUrl = currentTab.url;
   
-      const iframe = document.getElementById('iframe');
-      if (iframe) {
-        iframe.src = "http://127.0.0.1:5000/check/result/chrome?url="+currentUrl;
-      } else {
-        console.error('Element with ID "url" is not an iframe.');
-      }
-    });
+    const iframe = document.getElementById('iframe');
+
+    if (!iframe) {
+      console.error('Element with ID "iframe" is not found.');
+      return;
+    }
+
+    if (!currentUrl.startsWith("http") && !currentUrl.startsWith("https")){
+      iframe.src = "http://127.0.0.1:5000/error";
+      console.log("URL is not fine")
+    } else if (currentUrl.startsWith("chrome://") || currentUrl.startsWith("chrome")) {
+      iframe.src = "http://127.0.0.1:5000/error";
+      console.log("URL is not fine")
+    } else if (currentUrl.startsWith("http://192.168") || currentUrl.startsWith("http://127.")) {
+      iframe.src = "http://127.0.0.1:5000/error";
+      console.log("URL is not fine")
+    } else {
+      iframe.src = "http://127.0.0.1:5000/check/result/chrome?url=" + encodeURIComponent(currentUrl);
+      console.log("URL is fine")
+    }
   });
+});
