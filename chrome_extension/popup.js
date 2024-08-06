5271ab421004cf0280a608aa604c8494cb47e41e
Jiena Lee <december0two@gmail.com>
2024-08-06 16:03:08 -0700

    edited front end of website

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -13,19 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       var currentUrl = tabs[0].url;
       if (currentUrl.startsWith("https://mail.google.com")) {
-        iframe.src = "http://127.0.0.1:5000/gmail";
+        iframe.src = "http://127.0.0.1:8000/gmail";
         document.getElementById('message').style.display = 'block';
       } else if (!currentUrl.startsWith("http") && !currentUrl.startsWith("https")) {
-        iframe.src = "http://127.0.0.1:5000/error";
+        iframe.src = "http://127.0.0.1:8000/error";
         console.log("URL is not fine");
       } else if (currentUrl.startsWith("chrome://") || currentUrl.startsWith("chrome")) {
-        iframe.src = "http://127.0.0.1:5000/error";
+        iframe.src = "http://127.0.0.1:8000/error";
         console.log("URL is not fine");
       } else if (currentUrl.startsWith("http://192.168") || currentUrl.startsWith("http://127.")) {
-        iframe.src = "http://127.0.0.1:5000/error";
+        iframe.src = "http://127.0.0.1:8000/error";
         console.log("URL is not fine");
       } else {
-        iframe.src = "http://127.0.0.1:5000/check/result/chrome?url=" + encodeURIComponent(currentUrl);
+        iframe.src = "http://127.0.0.1:8000/check/result/chrome?url=" + encodeURIComponent(currentUrl);
         console.log("URL is fine");
       }
     })
