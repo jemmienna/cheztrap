3de715e9dccf1a81f19215cec9871b8fc3d116e4
Jiena Lee <december0two@gmail.com>
2024-10-23 22:48:38 -0700

    added reasoning

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,5 +1,5 @@
 const hostname = "https://cheztrap.com/";
-//const hostname = "http://127.0.0.1:8000/";
+// const hostname = "http://127.0.0.1:8000/";
 
 jQuery(document).ready(function () {
   function getPageDetails() {
@@ -35,7 +35,8 @@ jQuery(document).ready(function () {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentTab = tabs[0];
     const currentUrl = currentTab.url;
-
+    const urlElement = document.createElement("a");
+    urlElement.href = currentUrl;
     const gmailSections = [
       "#inbox",
       "#starred",
@@ -53,6 +54,9 @@ jQuery(document).ready(function () {
         currentUrl.startsWith("https://mail.google.com") &&
         currentUrl.endsWith(section)
     );
+
+    const websiteNameDisplay = $("#website_name");
+    const isSafeDisplay = $("#is_safe");
 
     let isGmailDetailPage =
       currentUrl.startsWith("https://mail.google.com") && !isGmailSection;
@@ -88,13 +92,26 @@ jQuery(document).ready(function () {
                 { title: title, bodyText: bodyText },
                 function (output) {
                   if (output.is_secure) {
-                    mainElement.html("<div style='margin-top: -15px;'><p style='font-size: 20px'>This gmail is safe</p><img src='/static/images/happy_cat.gif' style='width: 200px'></div>");
+                    websiteNameDisplay.html(
+                      "<div style='margin-top: -15px;'><p style='font-size: 20px'>This gmail is safe</p></div>"
+                    );
+                    isSafeDisplay.html(
+                      "<img src='https://cdn2.iconfinder.com/data/icons/greenline/512/check-128.png' style='width: 35px'><div style='margin-top: -15px;'><img src='/static/images/happy_cat.gif' style='width: 200px'></div><div style='font-size: 17.5px;'>" +
+                        output.reason +
+                        "</div>"
+                    );
                   } else {
-                    mainElement.html("<div style='font-size: 17.5px;'>This gmail may not be safe. Be careful!</div>");
+                    websiteNameDisplay.html(
+                      "<div style='font-size: 17.5px; margin-bottom: 10px;'>This gmail may not be safe. Be careful!</div>"
+                    );
+                    isSafeDisplay.html(
+                      "<img src='/static/images/bad.png' style='width: 35px;'><div style='font-size: 17.5px; margin-top: 10px;'>" +
+                        output.reason +
+                        "</div>"
+                    );
                   }
                 }
               );
-
             }
           }
         );
@@ -106,24 +123,36 @@ jQuery(document).ready(function () {
       currentUrl.startsWith("http://192.168") ||
       currentUrl.startsWith("http://127.")
     ) {
-
-      const websiteNameDisplay = $("#website_name")
-      const isSafeDisplay = $("#is_safe");
-
       $.post(
         hostname + "/api/check/website",
         { url: currentUrl },
         function (output) {
-          console.log("url", currentUrl)
+          console.log("output", output);
+
+          console.log(urlElement);
           if (output.is_secure) {
-            const urlElement = document.createElement('a');
-            urlElement.href = currentUrl
-            console.log(urlElement)
-            websiteNameDisplay.html("<div style='margin-bottom: 10px; font-size: 20px;'>" + urlElement.hostname + "<br> is a safe website</div>") 
-            console.log("thisl oaded but is still ading scren")
-            isSafeDisplay.html("<img src='https://cdn2.iconfinder.com/data/icons/greenline/512/check-128.png' style='width: 35px'><div style='margin-top: -15px;'><img src='/static/images/happy_cat.gif' style='width: 200px'></div>");
+            websiteNameDisplay.html(
+              "<div style='margin-bottom: 10px; font-size: 20px;'>" +
+                urlElement.hostname +
+                "<br> is a safe website</div>"
+            );
+            console.log("thisl oaded but is still ading scren");
+            isSafeDisplay.html(
+              "<img src='https://cdn2.iconfinder.com/data/icons/greenline/512/check-128.png' style='width: 35px'><div style='margin-top: -15px;'><img src='/static/images/happy_cat.gif' style='width: 200px'></div><div style='font-size: 17.5px;'>" +
+                output.reason +
+                "</div>"
+            );
           } else {
-            mainElement.html("<div style='font-size: 17.5px;'>This website may not be safe. Be careful!</div>");
+            websiteNameDisplay.html(
+              "<div style='margin-bottom: 10px; font-size: 20px;'>" +
+                urlElement.hostname
+            );
+            isSafeDisplay.html(
+              "<div><img src='/static/images/bad.png' style='width: 35px'></div><div style='font-size: 17.5px;'>This website may not be safe. Be careful!</div>" +
+                "<div style='font-size: 17.5px;margin-top: 10px;'>" +
+                output.reason +
+                "</div>"
+            );
           }
         }
       );
